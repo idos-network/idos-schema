d5856594ce8cbf5fb8b6eec9cd2d8d83a30287c9
Alexandr Kovalenko <alex.sapience@gmail.com>
2026-01-23 14:14:30 +0300

    Remove token bridges from main sql file

--- schema.sql
+++ schema.sql
@@ -7,18 +7,6 @@ REVOKE IF GRANTED SELECT ON main FROM default;
 
 USE IF NOT EXISTS idos AS idos;
 
-USE IF NOT EXISTS erc20 {
-    chain: 'arbitrumsepolia',
-    escrow: '0x52DE0ea92Fe07074D053a3216b391C412d5b548f',
-    distribution_period: '10m'
-} AS idos_token_bridge;
-
-USE IF NOT EXISTS erc20 {
-    chain: 'arbitrumsepolia',
-    escrow: '0x581A97E0D924b23DE5A641ec354739478d0900e2',
-    distribution_period: '10m'
-} AS usdc_token_bridge;
-
 
 -- TABLES
 
