04142ecf782d1d7fb9a884c0bbec1ec981bc2d50
Alexandr Kovalenko <alex.sapience@gmail.com>
2026-03-11 18:16:12 +0300

    Migrate to new escrows

--- schema_addons_staging.sql
+++ schema_addons_staging.sql
@@ -2,12 +2,12 @@
 
 USE IF NOT EXISTS erc20 {
     chain: 'arbitrum_sepolia',
-    escrow: '0x52DE0ea92Fe07074D053a3216b391C412d5b548f',
+    escrow: '0x1Cb4879cD8B714FC6c98a49248451C2Cc55b64E1',
     distribution_period: '10m'
 } AS idos_token_bridge;
 
 USE IF NOT EXISTS erc20 {
     chain: 'arbitrum_sepolia',
-    escrow: '0x581A97E0D924b23DE5A641ec354739478d0900e2',
+    escrow: '0x3593DA8aEEffc4Af3DC63122d23b77087C8892F7',
     distribution_period: '10m'
 } AS usdc_token_bridge;
