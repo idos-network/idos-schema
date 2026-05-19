3059986c5ae3b3e5a8fd38d5db74252cec7dd340
Alexandr Kovalenko <alex.sapience@gmail.com>
2026-05-19 16:24:37 +0300

    Add missed indexes

---
+++ migrations/ipfs/6.preliminary_credentials_indexes.sql
@@ -0,0 +1,4 @@
+CREATE INDEX IF NOT EXISTS preliminary_credentials_original_id_copy_id ON preliminary_credentials(original_id, copy_id);
+CREATE INDEX IF NOT EXISTS preliminary_credentials_copy_id ON preliminary_credentials(copy_id);
+CREATE INDEX IF NOT EXISTS preliminary_credentials_original_content_uri ON preliminary_credentials(original_content_uri);
+CREATE INDEX IF NOT EXISTS preliminary_credentials_copy_content_uri ON preliminary_credentials(copy_content_uri);
--- schema.sql
+++ schema.sql
@@ -82,6 +82,10 @@ CREATE TABLE IF NOT EXISTS preliminary_credentials (
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 CREATE INDEX IF NOT EXISTS preliminary_credentials_user_id ON preliminary_credentials(user_id);
+CREATE INDEX IF NOT EXISTS preliminary_credentials_original_id_copy_id ON preliminary_credentials(original_id, copy_id);
+CREATE INDEX IF NOT EXISTS preliminary_credentials_copy_id ON preliminary_credentials(copy_id);
+CREATE INDEX IF NOT EXISTS preliminary_credentials_original_content_uri ON preliminary_credentials(original_content_uri);
+CREATE INDEX IF NOT EXISTS preliminary_credentials_copy_content_uri ON preliminary_credentials(copy_content_uri);
 
 CREATE TABLE IF NOT EXISTS shared_credentials (
     original_id UUID NOT NULL,
