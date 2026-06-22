58f6e5e5608e96a006e332e5fe7f4cc0413aea64
Alexandr Kovalenko <alex.sapience@gmail.com>
2026-06-22 17:19:03 +0300

    Add index fro created_at on premilinary_credentials  table

--- migrations/ipfs/6.preliminary_credentials_indexes.sql
+++ migrations/ipfs/6.preliminary_credentials_indexes.sql
@@ -2,3 +2,4 @@ CREATE INDEX IF NOT EXISTS prelim_cred_original_id_copy_id ON preliminary_creden
 CREATE INDEX IF NOT EXISTS prelim_cred_copy_id ON preliminary_credentials(copy_id);
 CREATE INDEX IF NOT EXISTS prelim_cred_original_content_uri ON preliminary_credentials(original_content_uri);
 CREATE INDEX IF NOT EXISTS prelim_cred_copy_content_uri ON preliminary_credentials(copy_content_uri);
+CREATE INDEX IF NOT EXISTS prelim_cred_created_at ON preliminary_credentials(created_at);
